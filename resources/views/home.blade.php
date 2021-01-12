96dee69cff0d52d4094ebfe2942a3f12a1a39f1c
Kams Kanagi <karamo.kanagi@dodoker.com>
2021-01-12 23:16:23 +0800

    UI design

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,40 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="row ">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+        <div class="col-3">
+            <img src="https://i1.sndcdn.com/avatars-000326709935-8bqnrw-t500x500.jpg" alt="" style= "width: 100%; height: auto;" class="rounded-circle">
+        </div>
+        <div class="col-9 pt-5">
+            <div class="d-flex justify-content-between align-items-baseline">
+            <h1>user name</h1>
+            <a href="#">Add New Post</a>
+            </div>
+            <div class="d-flex">
+                <div class="pr-3"><strong>153</strong> posts</div>
+                <div class="pr-3"><strong>23</strong> followers</div>
+                <div class="pr-3"><strong>212</strong> following</div>
+            </div>
+            <div>
+                <div class="pt-4 font-weight-bold">title</div>
+                <div class=""><a href="">{{$user->profile->url ?? 'N/A'}}</a></div>
             </div>
         </div>
+    </div>
+
+
+    <div class="row pt-4">
+        <div class="col-4">
+                <img src="https://i1.sndcdn.com/avatars-000326709935-8bqnrw-t500x500.jpg" alt="" class="w-100">
+        </div>
+        <div class="col-4">
+                <img src="https://i1.sndcdn.com/avatars-000326709935-8bqnrw-t500x500.jpg" alt="" style= "width: 100%; height: auto;">
+        </div>
+        <div class="col-4">
+                <img src="https://i1.sndcdn.com/avatars-000326709935-8bqnrw-t500x500.jpg" alt="" style= "width: 100%; height: auto;">
+        </div>
+
     </div>
 </div>
 @endsection
